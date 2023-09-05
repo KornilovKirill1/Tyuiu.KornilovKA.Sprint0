15e83eaefffcf43e965ad8b1a4c54f58709b677b
kilianAid <kilian_bs@mail.ru>
2023-09-05 16:19:29 +0500

    Task1

---
+++ Tyuiu.KornilovKA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KornilovKA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Кирилл";
+            int age = 20;
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Здарствуйте, меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
